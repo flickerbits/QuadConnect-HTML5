42bafa42d561d2626b9996596dc2a1815b80f158
Hugh <hughsuh@gmail.com>
2017-01-16 03:19:45 -0600

    code cleanup

--- main.js
+++ main.js
@@ -15,13 +15,6 @@ window.onload = function() {
   canvasContext = canvas.getContext('2d');
   canvasContext.fillStyle = bg;
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
-  // var framesPerSecond = 30;
-  // setInterval(function() {
-  //   moveAll();
-  //   drawAll();
-  // }, 1000 / framesPerSecond);
-
   playerPiece = new GamePiece(40, 95, currentPlayer, 2);
   playerPiece.show();
   reset();
@@ -35,10 +28,6 @@ window.onload = function() {
       if (evt.key === 'ArrowLeft') { playerPiece.move(-1) };
       redrawAll();
     });
-}
-
-function moveAll() {
-
 }
 
 function redrawAll() {
@@ -58,7 +47,7 @@ function reset() {
   winner = false;
   x = 40;
   y = 455;
-  // fill columns with 'empty' gray game pieces to initialize game
+  // fill columns with 'empty' grey game pieces to initialize game
   for (var i = 0; i < 7; i++) {
     columns[i]= [];
     for (var j = 0; j < 6; j++) {
