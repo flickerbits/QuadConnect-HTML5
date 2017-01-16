790131fd2281f8265aa76161f22c75f65afd2677
Hugh <hughsuh@gmail.com>
2017-01-16 03:59:37 -0600

    comment cleanup

--- gamePiece.js
+++ gamePiece.js
@@ -2,16 +2,16 @@ function GamePiece(x, y, player, stroke) {
   this.x = x;
   this.y = y;
   this.player = player;
-  this.radius = 24;
+  this.radius = 25;
   this.stroke = stroke;
 
   this.show  = function() {
     var color;
-    if (this.player === 1) { color = 'red' }; // player 1 = RED
-    if (this.player === -1) { color = 'yellow' }; // player 2 = YELLOW
-    if (this.player === 0) { color = 'grey' }; // empty space = GREY
-    this.drawCircle(this.x, this.y, this.radius + this.stroke, 'black'); // draw stroke
-    this.drawCircle(this.x, this.y, this.radius, color); // draw gamePiece
+    if (this.player === 1) { color = 'red' };                           // player 1 = RED
+    if (this.player === -1) { color = 'yellow' };                       // player 2 = YELLOW
+    if (this.player === 0) { color = 'grey' };                          // empty space = GREY
+    this.drawCircle(this.x, this.y, this.radius, 'black');              // draw stroke
+    this.drawCircle(this.x, this.y, this.radius - this.stroke, color);  // draw gamePiece
   }
 
   this.move = function(dir) {
